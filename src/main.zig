75f21abc14eea3a2a5cbd216f5f1da17aaaf319b
CrepeGoat <studentoflife96@gmail.com>
2022-12-20 18:10:40 -0700

    day1part2 (simple)

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
-const src = @import("./day1part1.zig");
-
+const src = @import("./day1part2.zig");
 
 pub fn main() anyerror!void {
     try src.main(std.io.getStdIn().reader(), std.io.getStdOut().writer());
